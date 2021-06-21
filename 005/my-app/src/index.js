291c0d5eed687222f7b7ac51f12b07300e0b9ff3
Fernando Daciuk <f.daciuk@gmail.com>
2021-06-21 15:57:41 -0300

    005: DevTools, Fragment and JSX alignment

--- 005/my-app/src/index.js
+++ 005/my-app/src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+
+function Title () {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <span>Hello world</span>
+    </>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <Title />,
+  document.querySelector('#root')
+)
