d9d454995f2585d773a73384755353ddb6f6e4c5
Fernando Daciuk <f.daciuk@gmail.com>
2021-06-23 17:24:34 -0300

    Dealing with lists (arrays)

--- 008/my-app/src/app.js
+++ 008/my-app/src/app.js
@@ -1,40 +1,30 @@
+const menu = [
+  {
+    id: 'home',
+    name: 'Home',
+    link: '/',
+  },
+  {
+    id: 'home-mobile',
+    name: 'Home',
+    link: '/mobile-home',
+  },
+  {
+    id: 'sobre',
+    name: 'Sobre',
+    link: '/sobre'
+  }
+]
+
 function App () {
   return (
-    <>
-      <Title>Título</Title>
-      <Sidebar />
-
-      <Content />
-    </>
-  )
-}
-
-function Title ({ children }) {
-  return (
-    <h1>{children}</h1>
-  )
-}
-
-function Title2 ({ children }) {
-  return (
-    <h2>{children}</h2>
-  )
-}
-
-function Sidebar () {
-  return (
-    <aside>
-      <Title2>Sidebar</Title2>
-    </aside>
-  )
-}
-
-function Content () {
-  return (
-    <section>
-      <h2>Conteúdo</h2>
-      <p>Parágrafo</p>
-    </section>
+    <ul>
+      {menu.map((item) => (
+        <li key={item.id}>
+          <a href={item.link}>{item.name}</a>
+        </li>
+      ))}
+    </ul>
   )
 }
 
