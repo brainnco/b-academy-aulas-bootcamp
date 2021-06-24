26ce348b238a57dac9012fb809789079eb731e60
Fernando Daciuk <f.daciuk@gmail.com>
2021-06-24 17:10:41 -0300

    009: Introduction to events

--- 009/my-app/src/app.js
+++ 009/my-app/src/app.js
@@ -1,30 +1,10 @@
-const menu = [
-  {
-    id: 'home',
-    name: 'Home',
-    link: '/',
-  },
-  {
-    id: 'home-mobile',
-    name: 'Home',
-    link: '/mobile-home',
-  },
-  {
-    id: 'sobre',
-    name: 'Sobre',
-    link: '/sobre'
-  }
-]
-
 function App () {
+  function handleClick () {
+    alert('clicou!')
+  }
+
   return (
-    <ul>
-      {menu.map((item) => (
-        <li key={item.id}>
-          <a href={item.link}>{item.name}</a>
-        </li>
-      ))}
-    </ul>
+    <button onClick={handleClick}>Clique aqui!</button>
   )
 }
 
