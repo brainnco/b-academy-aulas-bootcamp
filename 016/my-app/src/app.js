02e1d5e6b19741934a8646cf0682655e8d64026d
Fernando Daciuk <f.daciuk@gmail.com>
2021-07-05 16:28:21 -0300

    016: How to make requests when component mounts

--- 016/my-app/src/app.js
+++ 016/my-app/src/app.js
@@ -1,16 +1,22 @@
+import { useEffect } from 'react'
+
 const url = 'https://ws.apicep.com/cep.json?code=[CEP]'
 
 function App () {
-  async function handleClick () {
-    console.log('buscando cep...')
-    const response = await fetch(url.replace('[CEP]', '06233-03'))
-    const json = await response.json()
-    console.log('cep:', json)
-  }
+  useEffect(() => {
+    async function getCep () {
+      console.log('buscando cep...')
+      const response = await fetch(url.replace('[CEP]', '06233-030'))
+      const json = await response.json()
+      console.log('cep:', json)
+    }
+
+    getCep()
+  })
 
   return (
     <>
-      <button onClick={handleClick}>Buscar CEP</button>
+      <button>Buscar CEP</button>
     </>
   )
 }
