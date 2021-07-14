ccee738f2373cbdb81eb92921402b58b31387561
Fernando Daciuk <f.daciuk@gmail.com>
2021-07-14 17:01:23 -0300

    Add es modules example

---
+++ 000-aulas-complementares/es-modules/vanilla-js/calculator.js
@@ -0,0 +1,12 @@
+function sum (a, b) {
+  return a + b
+}
+
+function mult (a, b) {
+  return a * b
+}
+
+const obj = { message: 'calculator' }
+
+export { sum, mult }
+export default obj
---
+++ 000-aulas-complementares/es-modules/vanilla-js/main.js
@@ -0,0 +1,4 @@
+import * as calculator from './calculator.js'
+
+console.log('default:', calculator.default)
+console.log('sum:', calculator.sum(1, 2))
