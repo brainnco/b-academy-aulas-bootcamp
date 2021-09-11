a4cbbcc045d04f9850523eb1babd8a358e1e26bf
Fernando Daciuk <f.daciuk@gmail.com>
2021-09-11 19:48:07 -0300

    026: Clean up app

--- 026/markee-app/src/app.tsx
+++ 026/markee-app/src/app.tsx
@@ -1,14 +1,6 @@
-import * as S from './app-styles'
-
 function App () {
   return (
-    <>
-      <S.Title>
-        App {' '}
-        <S.Span>texto do span</S.Span> {' '}
-        <S.SpanStyled>texto itálico</S.SpanStyled> {' '}
-      </S.Title>
-    </>
+    <h1>App</h1>
   )
 }
 
