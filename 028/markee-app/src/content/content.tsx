15045625256b3219a40aeace953e10e682f49a2b
Fernando Daciuk <f.daciuk@gmail.com>
2021-09-12 14:15:08 -0300

    Add 028

---
+++ 028/markee-app/src/content/content.tsx
@@ -0,0 +1,45 @@
+import { useState, ChangeEvent } from 'react'
+import * as S from './content-style'
+import marked from 'marked'
+
+import 'highlight.js/styles/github.css'
+
+import('highlight.js').then(hljs => {
+  const h = hljs.default
+
+  marked.setOptions({
+    highlight: (code, language) => {
+      if (language && h.getLanguage(language)) {
+        return h.highlight(code, { language }).value
+      }
+
+      return h.highlightAuto(code).value
+    },
+  })
+})
+
+export function Content () {
+  const [content, setContent] = useState('')
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value)
+  }
+
+  return (
+    <S.ContentWrapper>
+      <S.Header>
+        <S.Input defaultValue='Sem título' />
+      </S.Header>
+
+      <S.ContentSection>
+        <S.Textarea
+          placeholder='Digite aqui seu markdown'
+          value={content}
+          onChange={handleChange}
+        />
+
+        <S.Article dangerouslySetInnerHTML={{ __html: marked(content) }} />
+      </S.ContentSection>
+    </S.ContentWrapper>
+  )
+}
